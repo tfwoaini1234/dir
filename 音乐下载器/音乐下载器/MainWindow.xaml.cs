3460f30d1dce82266eafdc5458bb96bd23f9cc4f
chobits <zxchobits@outlook.com>
2019-12-18 23:12:22 +0800

    22222

--- 音乐下载器/音乐下载器/MainWindow.xaml.cs
+++ 音乐下载器/音乐下载器/MainWindow.xaml.cs
@@ -30,7 +30,9 @@ namespace 音乐下载器
         private string savePath = "";//保存目录
         private int page = 1;//起始页数
         List<Object> lists = new List<Object>();//搜索结果集合
-        int[] selectLists = { };
+        List<int> selectLists = new List<int>();
+        List<Object> downList = new List<object>();
+        int downType = 0;// 0未下载 1正在下载中 2下载完成
         public MainWindow()
         {
             InitializeComponent();
@@ -45,7 +47,7 @@ namespace 音乐下载器
         private void checkBox1_Click(object sender, RoutedEventArgs e)
         {
             var checkBox = sender as CheckBox;
-            selectLists.Append(int.Parse(checkBox.Tag.ToString()));
+            selectLists.Add(int.Parse(checkBox.Tag.ToString()));
         }
 
         /**
@@ -203,10 +205,10 @@ namespace 音乐下载器
             response = request.GetResponse();
             if (response == null) return;
             //读远程文件的大小
-            pbDown.Maximum = response.ContentLength;
+            //pbDown.Maximum = response.ContentLength;
             //下载远程文件
-            ThreadPool.QueueUserWorkItem((obj) =>
-            {
+            //ThreadPool.QueueUserWorkItem((obj) =>
+            //{
                 Stream netStream = response.GetResponseStream();
                 Stream fileStream = new FileStream(save_url, FileMode.Create);
                 byte[] read = new byte[1024];
@@ -216,13 +218,19 @@ namespace 音乐下载器
                 {
                     fileStream.Write(read, 0, realReadLen);
                     progressBarValue += realReadLen;
-                    pbDown.Dispatcher.BeginInvoke(new ProgressBarSetter(SetProgressBar), progressBarValue);
+                    //pbDown.Dispatcher.BeginInvoke(new ProgressBarSetter(SetProgressBar), progressBarValue);
                     realReadLen = netStream.Read(read, 0, read.Length);
                 }
                 netStream.Close();
                 fileStream.Close();
+                //下载进度+1
+                this.Dispatcher.Invoke(new Action(delegate
+                {
+                    //这里写代码
+                    pbDown.Value += 1;
+                }));
 
-            }, null);
+            //}, null);
         }
         /// <summary>
         ///  判断远程文件是否存在
@@ -319,13 +327,67 @@ namespace 音乐下载器
          */
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            if(savePath == "")
+            if (savePath == "")
             {
                 MessageBox.Show("请先选择保存地址", "提示");
             }
             //获取被选择的集合
-            pbDown.Maximum = selectLists.Length;
-            pbDown.Value = 2;
+
+            addDown();
+            pbDown.Maximum = selectLists.Count;
+            pbDown.Value = 0;
+            downListF();
+        }
+
+        private void addDown() {
+            foreach(int id in selectLists)
+            {
+                foreach(object item in lists)
+                {
+                    int id2 = int.Parse(item.GetType().GetProperty("id").GetValue(item).ToString());
+                    string url = item.GetType().GetProperty("url").GetValue(item).ToString();
+                    string name = item.GetType().GetProperty("name").GetValue(item).ToString();
+                    if (id == id2)
+                    {
+                        object obj = new { url = url, name = name };
+                        downList.Add(item);
+                    }
+                }
+            }
+        }
+        private void downListF()
+        {
+            ThreadPool.QueueUserWorkItem(obj =>
+            {
+                foreach(object item in downList)
+                {
+                    string url = item.GetType().GetProperty("url").GetValue(item).ToString();
+                    string name = item.GetType().GetProperty("name").GetValue(item).ToString();
+                    string bt = item.GetType().GetProperty("bt").GetValue(item).ToString();
+                    string ext = item.GetType().GetProperty("ext").GetValue(item).ToString();
+                    //判断文件是否存在
+                    FileInfo file = new FileInfo(savePath +"\\"+ name+"_"+bt+"."+ext);
+                    if (!file.Exists)
+                    {
+                        DownloadHttpFile(url, savePath + "\\" + name + "_" + bt + "." + ext);
+                    }
+                    else
+                    {
+                        this.Dispatcher.Invoke(new Action(delegate
+                        {
+                            //这里写代码
+                            pbDown.Value +=1;
+                        }));
+                    }
+                }
+                this.Dispatcher.Invoke(new Action(delegate
+                {
+                    //这里写代码
+                    pbDown.Value = pbDown.Maximum;
+                }));
+                
+                
+            });
         }
     }
 
@@ -338,5 +400,17 @@ namespace 音乐下载器
         public string hash { get; set; }
 
         public string hash320 {get; set;}
+    }
+
+    class downObj
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string size { get; set; }
+        public string time { get; set; }
+        public string url { get; set; }
+        public string ext { get; set; }
+        public string bt { get; set; }
+
     }
 }
